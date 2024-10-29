07dc8fe56f547430b7555d118fefe2effdac8e75
romain <ciszewiczromain@gmail.com>
2024-10-29 13:24:07 +0100

    modif heure tache encore

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('generate-sitemap');
+        $schedule->command('generate-sitemap')->dailyAt('12:30');
     }
 
     /**
